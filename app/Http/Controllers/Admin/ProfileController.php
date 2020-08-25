782e693c18bd552f4916443c236a3f11c62bec71
斉藤 <rs.0122.ssthe2@hotmail.co.jp>
2020-08-25 02:38:22 +0000

    php14 commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profiles;
+
 class ProfileController extends Controller
 {
     //
@@ -13,8 +15,15 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, Profiles::$rules);
+        $profiles = new Profiles;
+        $form = $request->all();
+        unset($form['_token']);
+        $profiles->fill($form);
+        $profiles->save();
+        
         return redirect('admin/profile/create');
     }
     
